e6312b8ecb76e4370346bd9030689f0a9597b7a5
Ian Clark <idclark13@gmail.com>
2013-10-19 14:33:00 -0400

    repetitive functions, about to be refactored

--- users.py
+++ users.py
@@ -32,3 +32,36 @@ def get_user_comments(client, user, sort='top', limit=25, t=all):
     response = client.get(url, data=user_information)
     comments = response.content
     return comments
+
+
+def get_liked(user, sort='top', limit=25, t=all):
+    user_information = {'user': user, 'sort': sort, 'limit': limit, 't': str(t)}
+    url = r'http://www.reddit.com/user/{u}/comments.json'.format(u=user)
+    response = client.get(url, data=user_information)
+    comments = response.content
+    return comments
+
+
+
+def get_disliked(user, sort='top', limit=25, t=all):
+    user_information = {'user': user, 'sort': sort, 'limit': limit, 't': str(t)}
+     url = r'http://www.reddit.com/user/{u}/comments.json'.format(u=user)
+    response = client.get(url, data=user_information)
+    comments = response.content
+    return comments
+
+
+def get_hidden(user, sort='top', limit=25, t=all):
+    user_information = {'user': user, 'sort': sort, 'limit': limit, 't': str(t)}
+    user, sort='top', limit=25, t=all url = r'http://www.reddit.com/user/{u}/comments.json'.format(u=user)
+    response = client.get(url, data=user_information)
+    comments = response.content
+    return comments
+    
+
+def get_saved(user, sort='top', limit=25, t=all):
+    user_information = {'user': user, 'sort': sort, 'limit': limit, 't': str(t)}
+    url = r'http://www.reddit.com/user/{u}/comments.json'.format(u=user)
+    response = client.get(url, data=user_information)
+    comments = response.content
+    return comments
